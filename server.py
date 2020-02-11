5be349ac421161b71b175ba5dc7704d675326905
Maximilian Richt <maxi@richt.name>
2020-02-11 19:52:49 +0100

    transfer location and battery_voltage

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import os
+import binascii
+import requests
+import jsons
 from datetime import datetime, timezone
 from twisted.internet import protocol, reactor, endpoints
 from twisted.protocols.basic import LineReceiver
@@ -6,8 +9,18 @@ from packet import Packet
 
 HOST = os.environ.get('HOST', '127.0.0.1')
 PORT = int(os.environ.get('PORT', '21105'))
+ENDPOINT = os.environ['ENDPOINT']
+ENDPOINT_AUTH_HEADER = os.getenv('ENDPOINT_AUTH_HEADER', '')
+
+headers = {
+    'Content-Type': 'application/json'
+}
+if ENDPOINT_AUTH_HEADER is not '':
+    headers['Authorization'] = ENDPOINT_AUTH_HEADER
 
 class BL10(LineReceiver):
+    device_id = None
+
     def __init__(self):
         self.packet = Packet()
 
@@ -15,7 +28,7 @@ class BL10(LineReceiver):
         dt = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone().replace(microsecond=0).isoformat()
         if direction == '>':
             direction = '==>'
-        else: 
+        else:
             direction = '<=='
         print("%s %s %s" % (dt, direction, binascii.hexlify(packet), ))
 
@@ -24,16 +37,17 @@ class BL10(LineReceiver):
         try:
             data = self.packet.parse(line + b'\r\n')
             print(data)
-            print(data.protocol)
-            if str(data.protocol) == 'login':
+            proto = str(data.protocol)
+            print(proto)
+            if proto == 'login':
                 self.handleLogin(data)
-            elif str(data.protocol) == 'heartbeat':
+            elif proto == 'heartbeat':
                 self.handleHeartbeat(data)
-            elif str(data.protocol) == 'location':
+            elif proto == 'location':
                 self.handleLocation(data)
-            elif str(data.protocol) == 'alarm':
+            elif proto == 'alarm':
                 self.handleAlarm(data)
-            elif str(data.protocol) == 'information':
+            elif proto == 'information':
                 self.handleInformation(data)
             else:
                 self.handleUnknown(data)
@@ -56,15 +70,45 @@ class BL10(LineReceiver):
         resp = self.packet.build(dict(start=b"\x78\x78", fields=dict(value=dict(length=1+(6+1+0)+2+2, protocol=0x01, data=respdata, serial=serial))))
         self.write(resp)
 
+        self.device_id = str(data.data.imei)
+
+        update = {
+            'device_id': self.device_id
+        }
+        print(jsons.dumps(update))
+        resp = requests.post(ENDPOINT, headers=headers, data=jsons.dumps(update))
+        print(resp)
+
     def handleHeartbeat(self, data):
         serial = data.serial + 1
         resp = self.packet.build(dict(start=b"\x78\x78", fields=dict(value=dict(length=1+2+2, protocol=0x23, data=bytes(), serial=serial))))
         self.write(resp)
 
+        update = {
+            'device_id': self.device_id,
+            'battery_voltage': data.data.voltage
+        }
+        print(jsons.dumps(update))
+        resp = requests.post(ENDPOINT, headers=headers, data=jsons.dumps(update))
+        print(resp)
+
     def handleLocation(self, data):
         serial = data.serial + 1
         resp = self.packet.build(dict(start=b"\x79\x79", fields=dict(value=dict(length=1+2+2, protocol=0x32, data=bytes(), serial=serial))))
         self.write(resp)
+
+        update = {
+            'device_id': self.device_id
+        }
+        
+        if data.data.gps:
+            # FIXME: something something with the data.data.gps.cs.latitude, data.data.gps.cs.longitude flags
+            update['lat'] = data.data.gps.latitude
+            update['lng'] = data.data.gps.longitude
+
+        print(jsons.dumps(update))
+        resp = requests.post(ENDPOINT, headers=headers, data=jsons.dumps(update))
+        print(resp)
 
     def handleAlarm(self, data):
         serial = data.serial + 1
